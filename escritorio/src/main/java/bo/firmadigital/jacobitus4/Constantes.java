f9b684f19fe1e014590601c3e6cddb45c7534463
Juan Carlos Paredes <jparedes@adsib.gob.bo>
2025-07-22 15:16:38 -0400

    se actualizo la version del proyecto

--- escritorio/src/main/java/bo/firmadigital/jacobitus4/Constantes.java
+++ escritorio/src/main/java/bo/firmadigital/jacobitus4/Constantes.java
@@ -1,6 +1,6 @@
 package bo.firmadigital.jacobitus4;
 
 public class Constantes {
-    public static String VERSION = "1.3.2";
+    public static String VERSION = "1.3.3-beta2";
     public static Long COMPILACION = 1750861606896L;
 }
