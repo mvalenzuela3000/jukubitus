03e3cb4cc8e90ddf5dd3fd5be6255be7e898fb15
Juan Carlos Paredes <juan.paredes@agetic.gob.bo>
2026-03-25 22:29:12 -0400

    chore: eliminar código comentado

--- escritorio/src/main/java/bo/firmadigital/jacobitus4/jetty/localhost9000/AppRest.java
+++ escritorio/src/main/java/bo/firmadigital/jacobitus4/jetty/localhost9000/AppRest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bo.firmadigital.jacobitus4.jetty.localhost9000;
 
 import bo.firmadigital.jacobitus4.App;
@@ -17,10 +12,6 @@ import javax.ws.rs.core.MediaType;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
-/**
- *
- * @author ADSIB
- */
 @Path("/app")
 public class AppRest {
     @POST
--- escritorio/src/main/java/bo/firmadigital/jacobitus4/jetty/localhost9000/UsbRest.java
+++ escritorio/src/main/java/bo/firmadigital/jacobitus4/jetty/localhost9000/UsbRest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bo.firmadigital.jacobitus4.jetty.localhost9000;
 
 import java.io.IOException;
@@ -18,10 +13,6 @@ import org.codehaus.jackson.map.ObjectMapper;
 import bo.firmadigital.jacobitus4.jetty.JettyHelper;
 import bo.firmadigital.jacobitus4.jetty.localhost9000.servicios.UsbServicio;
 
-/**
- *
- * @author ADSIB
- */
 @Path("/usbdisk")
 public class UsbRest {
     @GET
@@ -29,59 +20,6 @@ public class UsbRest {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public String capturar() {
-        // JSONObject json = new JSONObject();
-        // try {
-        //     if (OS.isUnix()) {
-        //         Process p = Runtime.getRuntime().exec("lsblk --nodeps -o name,serial,type,tran");
-        //         JSONArray jsonArrayUsb = new JSONArray();
-        //         BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        //         String linea = null;
-        //         while ((linea = in.readLine()) != null) {
-        //             StringTokenizer stoken = new StringTokenizer(linea);
-        //             if (stoken.countTokens() >= 4 && !linea.replace(" ", "").equals("NAMESERIALTYPETRAN")) {
-        //                 JSONObject jsonUsb = new JSONObject();
-        //                 int ind = 0;
-        //                 while (stoken.hasMoreTokens()) {
-        //                     String tk = stoken.nextToken();
-        //                     switch (ind) {
-        //                         case 0:
-        //                             jsonUsb.put("name", tk);
-        //                             break;
-        //                         case 1:
-        //                             jsonUsb.put("serial", tk);
-        //                             break;
-        //                         case 2:
-        //                             jsonUsb.put("type", tk);
-        //                             break;
-        //                         case 3:
-        //                             jsonUsb.put("tran", tk);
-        //                             break;
-        //                     }
-        //                     ind++;
-        //                 }
-        //                 if (jsonUsb.get("type").equals("disk") && jsonUsb.get("tran").equals("usb")) {
-        //                     jsonArrayUsb.put(jsonUsb);
-        //                 }
-        //             }
-        //         }
-        //         in.close();
-        //         JSONObject datos = new JSONObject();
-        //         datos.put("usbs", jsonArrayUsb);
-        //         json.put("datos", datos);
-        //     }
-        //     json.put("finalizado", true);
-        //     json.put("mensaje", "Operación finalizada correctamente...");
-        //     response.resume(json.toString());
-        // } catch (IOException | JSONException e) {
-        //     try {
-        //         json.put("datos", JSONObject.NULL);
-        //         json.put("finalizado", false);
-        //         json.put("mensaje", e.getMessage());
-        //     } catch (JSONException ex) {
-        //         Logger.getLogger(EstadoRest.class.getName()).log(Level.SEVERE, null, ex);
-        //     }
-        //     response.resume(json.toString());
-        // }
         UsbServicio servicio = new UsbServicio();
         ObjectMapper om = new ObjectMapper();
         try {
