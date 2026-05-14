71ed12438a8db44353f0bdb9b48779b945a840c8
Juan Carlos Paredes <juan.paredes@agetic.gob.bo>
2026-05-14 13:27:11 -0400

    fix: ajustar constantes

--- escritorio/src/main/java/bo/firmadigital/jacobitus4/jetty/localhost9000/servicios/EstadoServicio.java
+++ escritorio/src/main/java/bo/firmadigital/jacobitus4/jetty/localhost9000/servicios/EstadoServicio.java
@@ -3,7 +3,7 @@ package bo.firmadigital.jacobitus4.jetty.localhost9000.servicios;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import bo.firmadigital.jacobitus4.Constantes;
+import bo.firmadigital.jacobitus4.Informacion;
 import bo.firmadigital.jacobitus4.jetty.localhost9000.dtos.VersionDto;
 import bo.firmadigital.jacobitus4.jetty.localhost9000.dtos.comun.RespuestaDto;
 
@@ -16,8 +16,8 @@ public class EstadoServicio {
         RespuestaDto<VersionDto> respuesta = new RespuestaDto<VersionDto>();
         try {
             VersionDto datos = new VersionDto();
-            datos.setCompilacion(Constantes.COMPILACION);
-            datos.setApi_version(Constantes.VERSION);
+            datos.setCompilacion(Informacion.COMPILACION);
+            datos.setApi_version(Informacion.VERSION);
 
             respuesta.setDatos(datos);
             respuesta.setFinalizado(true);
