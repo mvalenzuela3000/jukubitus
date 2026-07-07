1cea7461617e85101a9f0f481b7785b8409f958b
Juan Carlos Paredes <juan.paredes@agetic.gob.bo>
2026-07-07 14:17:45 -0400

    feat: actualizar versión del proyecto

--- escritorio/src/main/java/bo/firmadigital/jacobitus/escritorio/Informacion.java
+++ escritorio/src/main/java/bo/firmadigital/jacobitus/escritorio/Informacion.java
@@ -2,8 +2,8 @@ package bo.firmadigital.jacobitus.escritorio;
 
 public class Informacion {
     public static final String NOMBRE = "jacobitus-escritorio";
-    public static final String VERSION = "1.4.0";
-    public static final Long COMPILACION = 1779893106343L;
+    public static final String VERSION = "2.0.0";
+    public static final Long COMPILACION = 1783448076058L;
 
     private Informacion() {}
 }
